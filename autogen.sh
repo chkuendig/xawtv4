0515a9e8eb111424c1baf28d861a45e408e1c859
kraxel <kraxel>
2004-02-24 21:08:19 +0000

    - various fixups for cvs.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-inst=$(ls	/usr/share/automake*/install-sh \
-		/usr/local/share/automake*/install-sh \
-		2>/dev/null | head -1)
+inst=$(echo /usr/share/automake*/install-sh | head -1)
 set -ex
 autoconf
 autoheader
