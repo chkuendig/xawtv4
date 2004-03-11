a39eecd4325146b2dbf8918f2a992f3059afb3f2
kraxel <kraxel>
2004-03-11 19:10:25 +0000

    - add files for dvb struct dumping.

---
+++ structs/struct-dvb.h
@@ -0,0 +1,15 @@
+extern char *dvb_fe_status[32];
+extern char *dvb_fe_caps[32];
+
+extern char *dvb_fe_type[];
+extern char *dvb_fe_bandwidth[];
+extern char *dvb_fe_rates[];
+extern char *dvb_fe_modulation[];
+extern char *dvb_fe_transmission[];
+extern char *dvb_fe_guard[];
+extern char *dvb_fe_hierarchy[];
+extern char *dvb_fe_inversion[];
+
+extern struct struct_desc desc_frontend_info[];
+
+extern struct ioctl_desc ioctls_dvb[256];
